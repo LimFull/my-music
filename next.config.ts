3dc21e058ec3d26c6c15ab3e78d999b0655c4bdf
LimFull <full7002@naver.com>
2025-03-23 12:35:01 +0900

    fix: build error

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,7 @@ import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
 
-    // Can be safely removed in newer versions of Next.js
-    future: {
-
-        // by default, if you customize webpack config, they switch back to version 4.
-        // Looks like backward compatibility approach.
-        webpack5: true,
-    },
-
+    
     webpack(config) {
         config.resolve.fallback = {
 
